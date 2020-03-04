1faeda00c4c06252822b88484b81d65c4c654722
YaaNuamah <yaa_fordjour@hotmail.com>
2020-03-04 18:52:54 +0000

    Job adding to the database

---
+++ src/main/java/io/turntabl/empireJob/StatusTO.java
@@ -0,0 +1,64 @@
+package io.turntabl.empireJob;
+
+
+
+public class StatusTO {
+
+    private String endpoint_url;
+    private Integer project_id;
+    private Integer status;
+    private Integer endpoint_id;
+    private String request_method;
+
+    public StatusTO() { }
+
+    public StatusTO(String endpoint_url, Integer project_id, Integer status, Integer endpoint_id, String request_method) {
+        this.endpoint_url = endpoint_url;
+        this.project_id = project_id;
+        this.status = status;
+        this.endpoint_id = endpoint_id;
+        this.request_method = request_method;
+    }
+
+    public String getEndpoint_url() {
+        return endpoint_url;
+    }
+
+    public void setEndpoint_url(String endpoint_url) {
+        this.endpoint_url = endpoint_url;
+    }
+
+    public Integer getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(Integer project_id) {
+        this.project_id = project_id;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getEndpoint_id() {
+        return endpoint_id;
+    }
+
+    public void setEndpoint_id(Integer endpoint_id) {
+        this.endpoint_id = endpoint_id;
+    }
+
+    public String getRequest_method() {
+        return request_method;
+    }
+
+    public void setRequest_method(String request_method) {
+        this.request_method = request_method;
+    }
+
+
+}
--- src/main/java/io/turntabl/empireJob/batchJob.java
+++ src/main/java/io/turntabl/empireJob/batchJob.java
@@ -1,5 +1,6 @@
 package io.turntabl.empireJob;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,33 +27,50 @@ public class batchJob {
 @Autowired
     JdbcTemplate template;
 
-    public  String getstatus(String url){
+    public static void getstatus(Integer project_id,String url, String method, Integer endpoint_id){
 
         String result = "";
-        int code = 200;
+        int code;
         try{
             URL siteURL = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) siteURL.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod(method);
             connection.setConnectTimeout(3000);
             connection.connect();
 
             code = connection.getResponseCode();
-            if (code == 200) {
+            postData(endpoint_id, project_id, code);
 
-                System.out.println("green \t" + url);
-
-            }
-            else {
-                System.out.println("red \t" + url);
-
-            }
         }
         catch (Exception e){
             System.out.println(e.getMessage());
         }
-        return  result;
     }
+
+    public static void  postData(Integer endpoint_id, Integer project_id, Integer status ) throws IOException, InterruptedException {
+        var values = new HashMap<String, Integer>() {{
+            put("endpoint_id", endpoint_id);
+            put("project_id", project_id);
+            put("status", status);
+        }};
+
+        var objectMapper = new ObjectMapper();
+        String requestBody = objectMapper
+                .writeValueAsString(values);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://192.168.8.122:8050/api/v1/addStatus"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
 
 
     @CrossOrigin
@@ -59,7 +83,7 @@ public class batchJob {
         );
 
         response.stream().map(endpoint -> endpoint.getEndpoint_url()).collect(Collectors.toList()).forEach(entry -> {
-            getstatus(entry);
+            getstatus(1, "asdasd",entry, 5);
         });
         return  response.stream().map(endpoint -> endpoint.getEndpoint_url()).collect(Collectors.toList());
     }
